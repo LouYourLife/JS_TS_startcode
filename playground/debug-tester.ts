d30e94b862ebd01f2842b1275ea05881341d1b81
LouYourLife <vibekenordestgaard@gmail.com>
2021-03-11 12:36:05 +0100

    startcode

---
+++ playground/debug-tester.ts
@@ -0,0 +1,14 @@
+import * as dotenv from "dotenv";
+dotenv.config()
+import debug from 'debug'
+const debug1 = debug('xxx:a')
+const debug2 = debug('xxx:b')
+const debug3 = debug('xxx:c')
+
+console.log("A1")
+debug1("hello")
+debug2("hello b")
+debug3(process.env.PORT)
+
+// "ts-node debug-tester.ts" in terminal, make sure to be in playground folder!
+// set DEBUG=xxx:* & ...
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path"
+
+dotenv.config()
+const app = express()
+
+// Gør at vi kan se/nå filer i public folder
+// Alle kan se hvad der ligger i public folderen
+//app.use('/static', express.static('public'))
+app.use(express.static(path.join(process.cwd(), "public")))
+
+// Something has to go in here
+ 
+app.get("/demo", (req, res) => {
+    let a = 124;
+    console.log(a);
+    res.send("Server is really up");
+})
+
+export default app;
